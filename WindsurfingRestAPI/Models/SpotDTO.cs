f0dcb7bef0f33c139c4da3c1e8f38ec253b6210b
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-01-24 20:32:01 +0100

    creating the models dto classes

---
+++ WindsurfingRestAPI/Models/SpotDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WindsurfingRestAPI.Models
+{
+    public class SpotDTO
+    {
+        public string Name {  get ; set; }  
+        public string country { get; set; } 
+        public string Description { get; set; } 
+        public int Hearts { get; set; } 
+        public virtual List<WindsurferDTO> WindsurfersDTO { get; set; }  
+    }
+}
---
+++ WindsurfingRestAPI/Models/WindsurferDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WindsurfingRestAPI.Models
+{
+    public class WindsurferDTO
+    {
+        public int ID { get; set;  }
+        public string Name { get; set;  } 
+        public string Age { get; set; }
+        public string Nationality { get; set; }    
+        public virtual List<SpotDTO> SpotsDTO { get; set; }
+    }
+}
