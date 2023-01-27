7b37848707c7f80fd51fe36e4b288cf6c7aaebbc
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-01-27 11:04:00 +0100

    finishing the automapper profile classes

--- WindsurfingRestAPI/Models/SpotDTO.cs
+++ WindsurfingRestAPI/Models/SpotDTO.cs
@@ -2,8 +2,8 @@
 {
     public class SpotDTO
     {
-        public string Name {  get ; set; }  = string.Empty;
-        public string country { get; set; } = string.Empty; 
+        public string NameCountry {  get ; set; }  = string.Empty;
+        //public string country { get; set; } = string.Empty;  it is called data transfert object !! try to specify your exact objective and data you need to show !! gottt concentrate more !!! 
         public string Description { get; set; } = string.Empty; 
         public int Hearts { get; set; } 
     }
---
+++ WindsurfingRestAPI/Profiles/SpotProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace WindsurfingRestAPI.Profiles
+{
+    public class SpotProfile : Profile
+    {
+        public SpotProfile()
+        {
+            CreateMap<Entities.Spot, Models.SpotDTO>()
+            .ForMember(dest => dest.NameCountry, opt =>
+            opt.MapFrom(src => $"{src.Name} {src.country}"))
+        .ForMember(dest => dest.Description, opt =>
+            opt.MapFrom(src => src.Description))
+        .ForMember(dest => dest.Hearts, opt =>
+            opt.MapFrom(src => src.Hearts));
+
+        }
+
+    }
+}
